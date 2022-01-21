ec4f26bdbe0093c3337ba9719533423a0b4e2a35
SecretShinobi <44608766+SecretShinobi@users.noreply.github.com>
2022-01-21 11:36:50 -0500

    implement complex_number.py
    implemented complex_number

--- complex_number.py
+++ complex_number.py
@@ -10,49 +10,70 @@ class ComplexNumber:
 
     === Sample Usage ===
     Creating a complex number:
-    >>> C = ComplexNumber(5, 3)
-    >>> C
-    '5i + 3'
-    >>> C0 = ComplexNumber(-4, -2)
-    >>> C0
-   '-4i - 2'
+    >>> c = ComplexNumber(5, 3)
+    >>> str(c)
+    '5i+3'
+    >>> c0 = ComplexNumber(-4, -2)
+    >>> str(c0)
+   '-4i-2'
     """
     real: Union[int, float]
     imaginary: Union[int, float]
 
-    def __init__(self, real: Union[int, float], imaginary: Union[int, float]) \
+    def __init__(self, imaginary: Union[int, float], real: Union[int, float]) \
             -> None:
         """Initialize a new complex number with a real component with value
         <real> and an imaginary component with value <imaginary>
 
-        >>> C = ComplexNumber(5, 7)
-        >>> C.real
+        >>> c = ComplexNumber(5, 7)
+        >>> c.real
         5
-        >>> C.imaginary
+        >>> c.imaginary
         7
         """
-        # TODO: Write implementation
-        pass
+        self.real = real
+        self.imaginary = imaginary
 
     def __str__(self) -> str:
         """Prints out a string representation of the complex number
 
-        >>> C = ComplexNumber(4, -9)
-        >>> C
-        '4i - 9'
+        >>> c = ComplexNumber(4, -9)
+        >>> c
+        '4i-9'
         """
-        # TODO: Write implementation
-        pass
+        if self.imaginary == -1:
+            return '-i' + self._string_helper()
+        return (str(self.imaginary) + 'i' + self._string_helper()).lstrip('1')
 
     def __eq__(self, other) -> bool:
         """Returns if the complex number <self> is equal in value to the complex
         number <other>
 
-        >>> C1 = ComplexNumber(-5, 6)
-        >>> C2 = ComplexNumber(5, 6)
-        >>> C1 == C2
-        False
+        >>> c1 = ComplexNumber(5, 6)
+        >>> c2 = ComplexNumber(5, 6)
+        >>> c1 == c2
+        True
         """
-        # TODO: Write implementation
-        pass
+        return (self.real == other.real) and (self.imaginary == other.imaginary)
 
+    def __ne__(self, other) -> bool:
+        """Returns if the complex number <self> is not equal in value to the
+        complex number <other>
+
+        >>> c1 = ComplexNumber(5, -6)
+        >>> c2 = ComplexNumber(-5, 6)
+        >>> c1 != c2
+        True
+        """
+        return (self.real != other.real) or (self.imaginary != other.imaginary)
+
+    def _string_helper(self) -> str:
+        """Private helper function for the __str__ magic method
+
+        Helps add a plus sign, minus sign, or no number in the string
+        """
+        if self.real > 0:
+            return '+' + str(self.real)
+        elif self.real < 0:
+            return str(self.real)
+        return ''
